fc9da9a2c7385cf609dcd5269af265217da90c5d
dungatoro <71789541+dungatoro@users.noreply.github.com>
2024-06-18 19:41:35 +0100

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -1,31 +1,5 @@
 from microbit import *
 
-def grid_to_image(grid):
-    return Image("".join(["".join(map(str, row))+':' for row in grid]))
-
-def tick(grid):
-    new = [ [0]*5 for _ in range(5) ]
-    for x in range(5):
-        for y in range(5):
-            count = sum(
-                [grid[i%5][j%5] for i, j in 
-                [(x-1,y), (x-1,y-1), (x,y-1), (x+1,y-1), (x+1,y), (x+1,y+1), (x,y+1), (x-1,y+1)]
-            ])//9
-
-            if count == 3: new[x][y] = 9
-            elif count == 2 and grid[x][y]: new[x][y] = 9
-            else: new[x][y] = 0
-     
-    return new
-            
-grid = [
-    [0,0,0,0,0],
-    [0,0,9,0,0],
-    [0,0,0,9,0],
-    [0,9,9,9,0],
-    [0,0,0,0,0],
-]
-
 x, y = 0, 0
 pixel = 0
 
@@ -37,14 +11,7 @@ def move():
     if y == 5:
         y = 0
         
-display.show(grid_to_image(grid))
 while True:
-    while button_a.is_pressed() and button_b.is_pressed():
-    
-
-        sleep(200)
-        grid = tick(grid)
-
     if button_a.was_pressed():
         display.set_pixel(x, y, pixel)
         move()
@@ -55,7 +22,6 @@ while True:
             display.set_pixel(x, y, 0)
         else:
             display.set_pixel(x, y, 9)
-            
         move()
         pixel = display.get_pixel(x, y)
 
